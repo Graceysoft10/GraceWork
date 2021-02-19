6dc933ef80827a6e9628017430f52c5055916425
Graceysoft10 <79143286+Graceysoft10@users.noreply.github.com>
2021-02-19 18:34:06 +0100

    Add files via upload
    java program on employee name and monthly salary and date.

---
+++ DateTest.java
@@ -0,0 +1,11 @@
+public class DateTest{
+  public static void main (String[] args){
+
+Date displayDate = new Date(10,25,2020);
+
+System.out.printf("The date is %s/%s/%s%n ", displayDate.getMonth(), displayDate.getDay(), displayDate.getYear());
+
+
+	}
+
+}
