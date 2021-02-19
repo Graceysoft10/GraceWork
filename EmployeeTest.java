d38c0d1b6de3b386ba1ac395849f42d1eb0280e7
Grace <funmibiajadi@gmail.com>
2021-02-19 13:56:39 +0100

    name and salary of the two employees, date of the year

---
+++ EmployeeTest.java
@@ -0,0 +1,77 @@
+public class EmployeeTest{
+  public static void main (String[] args){
+
+Employee gracey = new Employee("good", "great", 55.21);
+Employee gracei = new Employee("better","best", 45.10);
+
+
+	gracey.setFirstName("Grace");
+	gracey.setLastName("Ajadi");
+	gracey.setMonthlySalary(250.00);	
+	gracey.getFirstName();
+	gracey.getLastName();
+	gracey.getMonthlySalary();
+	gracey.getYearlySalary();
+	
+
+  	gracei.setFirstName("Jane");
+	gracei.setLastName("Green");
+	gracei.setMonthlySalary(200.00);	
+	gracei.getFirstName();
+	gracei.getLastName();
+	gracei.getMonthlySalary(); 
+	gracei.getYearlySalary(); 
+	
+	
+System.out.printf("My  employee1 first name is %s%n", gracey.getFirstName());
+System.out.printf("My employee1 last name is %s%n", gracey.getLastName());
+System.out.printf("Her monthly salary is $%.2f", gracey.getMonthlySalary());
+System.out.println();
+System.out.printf("Her yearly salary is $%.2f", gracey.getYearlySalary());
+System.out.println();
+	gracey.increment();
+System.out.println("Her monthly salary with 10% is " + gracey.getMonthlySalary());
+System.out.println("Her yearly salary is now " + gracey.getYearlySalary());
+
+
+System.out.println();
+
+
+System.out.printf("My employee2 first name is %s%n", gracei.getFirstName());
+System.out.printf("My employee2 last name is %s%n", gracei.getLastName());
+System.out.printf("Her monthly salary is $%.2f", gracei.getMonthlySalary());
+System.out.println();
+System.out.printf("Her yearly salary is $%.2f", gracei.getYearlySalary());
+	gracei.increment();
+System.out.println();
+System.out.println("Her monthly salary with 10 % is"+ gracei.getMonthlySalary());
+System.out.println("Her yearly salary is now " + gracei.getYearlySalary());
+	
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
